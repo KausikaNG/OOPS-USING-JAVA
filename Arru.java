534e255cee8a704933496d8df96c6f6ed01f565e
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:52:42 +0530

    PRINTING OF ARRAY

---
+++ Arru.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Arru
+{
+public static void main(String[] args)
+{
+int n,i;
+System.out.println("value of n");
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+System.out.println("enter the element");
+int a[]=new int[n];
+for(i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+for(i=0;i<n;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
