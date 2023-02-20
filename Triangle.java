861807cf303bb24cc5bfa514367dc950a67c5cfb
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:38:05 +0530

    TRIANGLE FORMED OR NOT

---
+++ Triangle.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+import java.util.*;
+public class Triangle
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int sum=s.nextInt();
+if(sum==180) 
+{
+System.out.println("triangle is formed");
+}
+else
+{
+System.out.println("triangle is not formed");
+}
+}
+}
