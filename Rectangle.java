daee8ff663f2722c848666561c74d6f3e7b680d6
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:26:10 +0530

    AREA OF RECTANGLE

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+import java.util.*;
+public class Rectangle
+{
+public static void main(String[] args)
+{
+
+Scanner b=new Scanner(System.in);
+int l,w,a;
+System.out.println("the number is");
+l = b.nextInt();
+Scanner c=new Scanner(System.in);
+System.out.println("the number is");
+w = c.nextInt();
+a= l * w;
+System.out.println(a);
+}
+}
