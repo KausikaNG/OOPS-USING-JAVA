9a6053d3be4ea10dda2b17455de29dda10ee6bcc
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:19:15 +0530

    PATTERN 2

---
+++ p.java
@@ -0,0 +1,33 @@
+if(i==1 || i==7)
+{
+System.out.print("@");
+}
+else if(i==2 && j==6  || i==2 && j==7 )
+{
+System.out.print("#");
+}
+else if(i==3 && j==5  || i==3 && j==6 )
+{
+System.out.print("#");
+}
+else if(i==4 && j==4  || i==4 && j==5 )
+{
+System.out.print("#");
+}
+else if(i==5 && j==4  || i==5 && j==5 )
+{
+System.out.print("#");
+}
+else if(i==6 && j==3  || i==6 && j==4 )
+{
+System.out.print("#");
+}
+else
+{
+
+System.out.print(" ");
+}
+}
+}
+}
+}
