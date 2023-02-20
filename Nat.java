7d5858b1a9433f5541c5e1a92214c8f1c3ba3533
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:13:39 +0530

    PRINTING OF NATURAL NUMBERS FROM USER

---
+++ Nat.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Nat
+{
+public static void main(String args[])
+{
+int n;
+System.out.println("enter the value of n");
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+int i;
+for(i=1;i<=n;i++)
+{
+System.out.printf("%d",i);
+}
+}
+}
