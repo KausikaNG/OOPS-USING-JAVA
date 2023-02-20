46e336e34b66e4bd92b98e8344904b36736fdd86
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:41:46 +0530

    ARRAY EQUAL OR NOT

---
+++ Arre.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Arrays;
+public class Arre
+{
+public static void main(String[] args)
+{
+int[] arr1=new int[] {1,2,3,4};
+int[] arr2=new int[] {1,2,3,4};
+System.out.println(Arrays.equals(arr1,arr2));
+}
+}
+
