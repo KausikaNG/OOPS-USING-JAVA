d2d38c7e755235f5b78dce7b36e60bcd82b02d29
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:56:23 +0530

    BOOLEAN TO INT

---
+++ Booint.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Booint
+{
+public static void main(String[] args)
+{
+int[] n1={2,3,12,4,12,-2};
+int[]n3=new int[5];
+int []n2=new int[n1.length];
+Boolean bool=true;
+System.out.println(bool);
+int val=(bool) ? 1:0;
+System.out.println(val);
+}
+}
