b77dfa5a01a094e13c95db1ea4b2c2ecd816f34f
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:55:46 +0530

    CONSTRUCTOR OVERLOADING

---
+++ Bite1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class Student4
+{
+int id;
+String num;
+Student4(int i,String n)
+{
+id=i;
+name=n;
+}
+void display()
+{
+System.out.println(i+" "+name);
+}
+public static void main(String[] args)
+{
+Student s1=new Student4(1,"arun");
+Student s1=new Student4(2,"swetha");
+s1.dispaly();
+s2.display();
+}
+}
