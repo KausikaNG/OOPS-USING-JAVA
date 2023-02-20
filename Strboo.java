efc659bf81df3d2fc192871840765ecff0e6b694
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:32:02 +0530

    STRING TO BOOLEAN

---
+++ Strboo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Strboo
+{
+public static void main(String[] args)
+{
+String str="false";
+Boolean bool=Boolean.parseBoolean(str);
+System.out.println(bool);
+}
+}
