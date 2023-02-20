da25cf15f2c0da2bce7b747409727f96509650a2
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:15:49 +0530

    STRING TO BOOLEAN

---
+++ Objboo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Objboo
+{
+public static void main(String[] args)
+{
+String str="true";
+Boolean bool=Boolean.valueOf(str);
+System.out.println(bool);
+}
+}
