525ece2ddb6e4b4c7eddcaa98ec834bab293efa3
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:59:58 +0530

    AREA OF CIRCLE

---
+++ Circle.java
@@ -0,0 +1,16 @@
+import java.io.PrintStream;
+import java.util.*;
+public class Circle
+{
+public static void main(String[] args)
+{
+
+Scanner b=new Scanner(System.in);
+int r;
+double pi=3.14,a;
+System.out.println("the number is");
+r = b.nextInt();
+a=pi * r * r;
+System.out.println(a);
+}
+}
