22dc564c48a53557d9f51be532035872bd770c49
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:21:12 +0530

    PATTERN 3

---
+++ Pat.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Pat
+{
+public static void main(String args[])
+{
+int i, j,n=5;
+for (i=5; i >= 1; --i)
+{
+for (j = 1; j <= i; ++j)
+{
+System.out.printf("* ");
+}
+System.out.printf("\n");
+}
+}
+}
+
