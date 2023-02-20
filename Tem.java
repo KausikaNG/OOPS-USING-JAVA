f93999aac48ed7e54ec7d9720afdd2040015f4b7
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:35:17 +0530

    CLECIUS TO FAHRENHEIT

---
+++ Tem.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Tem
+{
+public static void main(String[] args)
+{
+float celsius,fahrenheit;
+celsius=25;
+fahrenheit=((celsius*9)/5)+32;
+System.out.println("the fahrenheit is");
+System.out.println(fahrenheit);
+
+}
+}
