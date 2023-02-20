6f7ab1124784dd78d1bb54ff2c650de25213bedc
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:32:51 +0530

    CLASS OBJECT

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Student
+{
+int id=01;
+String name="kausika";
+float fees=10000f;
+public static void main(String[] args)
+{
+Student s=new Student();
+System.out.println(s.id);
+System.out.println(s.name);
+System.out.println(s.fees);
+}
+}
+
