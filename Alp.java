d905cf3afeb60cf106dcb26bb1eb4bd2aef6e955
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:38:50 +0530

    INPUT FROM USER

---
+++ Alp.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+import java.util.*;
+public class Alp
+{
+public static void main(String[] args)
+{
+Scanner b=new Scanner(System.in);
+System.out.println("enter tha string");
+string name = b.nextLine();
+System.out.println(name);
+}
+}
