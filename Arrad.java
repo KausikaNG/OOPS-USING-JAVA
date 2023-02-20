9ae8baf9834fa7afc8469e7a4057912bdbf1f2f6
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:40:42 +0530

    ADDITION OF TWO ARRAY

---
+++ Arrad.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class Arrad
+{
+public static void main(String[] args) 
+{
+Scanner sc=new Scanner(System.in);
+int row=sc.nextInt();
+int column=sc.nextInt();
+int a[][]=new int[row][column];
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<column;j++)
+{
+a[i][j]=sc.nextInt();
+}
+}
+System.out.print("Elements in Array are :\n");
+int sum=0;
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<column;j++)
+{
+sum+=a[i][j];
+}
+System.out.println(sum);
+}
+}
+}
+
