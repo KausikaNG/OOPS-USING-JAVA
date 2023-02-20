16f39541070b86ddaa32d59b89e55df59658510b
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:33:51 +0530

    addition of two numbers using scanner

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.io.PrintStream;
+import java.util.*;
+public class Add
+{
+public static void main(String[] args)
+{
+Scanner b=new Scanner(System.in);
+Scanner c=new Scanner(System.in);
+System.out.println("the first number is");
+int a = b.nextInt();
+System.out.println("the second number is");
+int x = c.nextInt();
+int z=a+x;
+System.out.println(z);
+}
+}
