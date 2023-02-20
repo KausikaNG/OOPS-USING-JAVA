e6bfc763d89a630e345e041898c105a88eddfe4a
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:57:12 +0530

    BOOLEAN TO STRING

---
+++ Bool.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Bool
+{
+public static void main(String[] args)
+{
+Boolean bool=false;
+String str = new Boolean(bool).toString();
+System.out.println(str);
+}
+}
