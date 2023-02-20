97fd8717a996d3cee3b4bc3c11b9d87920f4b58c
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:29:01 +0530

    SQUARE OF GIVEN NUMBER

---
+++ Square.java
@@ -0,0 +1,15 @@
+import java.io.PrintStream;
+import java.util.*;
+public class Square
+{
+public static void main(String[] args)
+{
+
+Scanner b=new Scanner(System.in);
+int s,a;
+System.out.println("the number is");
+s = b.nextInt();
+a= s * s;
+System.out.println(a);
+}
+}
