378e03644dc7c954f189371d368e06bb55717b1a
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 14:57:52 +0530

    BOOLEAN PRINTING

---
+++ Boolean.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+public class Boolean
+{
+public static void main(String[]args)
+{
+boolean Java=true;
+boolean Python=false;
+System.out.println(Java);
+System.out.println(Python);
+
+}
+}
