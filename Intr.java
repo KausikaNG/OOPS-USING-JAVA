03c5d9509df5f49046e807ff4cce380f6311212a
KausikaNG <124652747+KausikaNG@users.noreply.github.com>
2023-02-20 15:08:41 +0530

    ARRAY TO CHARACTER ARRAY

---
+++ Intr.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Intr
+{
+public static void main(String[] args)
+{
+int n;
+System.out.println("value of n");
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+System.out.println("enter the string");
+char a[]=s.next().toCharArray();
+System.out.println("enter the string ");
+char a[]=s.next().toCharArray();
